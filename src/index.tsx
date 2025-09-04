dad59a455d8f4b12da809523f741bbcf49824789
Carlos Eduardo <carloseduardolimabastosm@gmail.com>
2025-09-04 20:46:39 -0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  < >
+  </>
+);
