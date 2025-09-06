d2b1323268b1058f9987b32cdd247643da30c813
Carlos Eduardo <carloseduardolimabastosm@gmail.com>
2025-09-05 22:58:32 -0300

    Criando banco de dados

---
+++ src/api/db.ts
@@ -0,0 +1,279 @@
+export const data = [
+  {
+    "id": 0,
+    "title": "Camisa com logo",
+    "description": "Se todo o seu guarda-roupa é preto e pelo menos uma pessoa em sua vida lhe disse para usar mais cores, esta camisa é o compromisso que você estava esperando.",
+    "price": 40,
+    "category": "vestido",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-abir-hasan-1857375-removebg-preview.png",
+    "newProduct": true
+  },
+  {
+    "id": 1,
+    "title": "camiseta design in bloom",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 63.20,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-artem-podrez-7048257-removebg-preview.png",
+  },
+  {
+    "id": 2,
+    "title": "Jaqueta da história da versão",
+    "description": "100% poliéster, tamanho unissex. Estampado em uma jaqueta New Era.",
+    "price": 120.20,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-daniel-adesina-11351031-removebg-preview.png",
+  },
+  {
+    "id": 3,
+    "title": "Moletom Canguru Unissex",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 70,
+    "category": "moletom",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-danik-prihodko-15759565-removebg-preview.png"
+  },
+  {
+    "id": 4,
+    "title": "Elevated workflows tee",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 80.40,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-elijah-o_donnell-5303044-removebg-preview.png",
+    "newProduct": true
+  },
+  {
+    "id": 5,
+    "title": "camiseta design in bloom",
+    "description": "52/48 Airlume penteado e lã de algodão/poliéster torcido, marca bordada, tamanho unissex. Impresso em um moletom ",
+    "price": 50,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-gabriela-pereira-1844132-removebg-preview.png"
+  },
+  {
+    "id": 6,
+    "title": "Detach instance tee",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 40.50,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-gustavo-almeida-3302595-removebg-preview.png",
+    
+  },
+  {
+    "id": 7,
+    "title": "Boné",
+    "description": "Ajuste ajustável, alça de tecido com fecho de metal latão. Um tamanho se encaixa mais.",
+    "price": 40,
+    "category": "boné",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-jace-oner-12027083-removebg-preview.png"
+  },
+  {
+    "id": 8,
+    "title": "Detach instance tee",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 70,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-jill-burrow-6758073-removebg-preview.png"
+  },
+  {
+    "id": 9,
+    "title": "Camiseta Pixel Perfect Dark Mode",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 40.50,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-joshua-mcknight-1544725-removebg-preview.png"
+  },
+  {
+    "id": 10,
+    "title": "Vestido rosa",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 110,
+    "category": "vestido",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-kadeem-stewart-15787280-removebg-preview.png"
+  },
+  {
+    "id": 11,
+    "title": "Camiseta Pixel",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 50,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-lucas-pezeta-1963234-removebg-preview.png",
+    "newProduct": true
+
+  },
+  {
+    "id": 12,
+    "title": "Camiseta Pixel Perfect Dark Mode",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 56,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-lucas-pezeta-1963239-removebg-preview.png"
+  },
+  {
+    "id": 13,
+    "title": "Vestido rosa",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 130.99,
+    "category": "vestido",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-moose-photos-1036623-removebg-preview.png"
+  },
+  {
+    "id": 14,
+    "title": "Camiseta design in bloom",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 56,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-rômulo-carolino-2843486-removebg-preview.png"
+  },
+  {
+    "id": 15,
+    "title": "Vestido rosa",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 56,
+    "category": "camisa",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-sandro-tavares-15728332-removebg-preview.png"
+  },
+  {
+    "id": 16,
+    "title": "Camiseta branca",
+    "description": "100% algodão, tamanho unissex. Impresso com tinta puff em uma camiseta Comfort Colors.",
+    "price": 150,
+    "category": "vestido",
+    "sizes": [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "2XL"
+    ],
+    "image": "./assets/pexels-карина-каржавина-7825931-removebg-preview.png"
+  },
+]
